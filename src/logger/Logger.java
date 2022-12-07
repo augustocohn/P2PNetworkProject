a8c1baf168284c3e0690e2aa1d1a02d0d366716f
Augusto Cohn <augustocohn@ufl.edu>
2022-12-07 14:06:56 -0500

    filled in the trivial definitions

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -45,40 +45,49 @@ public class Logger {
     public void changePreferredNeighborsLog(int connectedPeerID) throws IOException {
         String toWrite = generateCurrentDateTime() + " : Peer " + this.peerID + " has the list of preferred neighbors ";
 //        for(Peer peer : Peer.getPeerByID(this.peerID).get) //write a method to get the preferred neighbors (need the data structure and add and clear in TimerTask
+
         this.fileWriter.write(toWrite);
     }
 
     public void changeOptimisticallyUnchokedLog(int connectedPeerID) throws IOException {
-
+        //uh oh spaghetti o
     }
 
     public void unchokeByLog(int connectedPeerID) throws IOException {
-
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " is unchoked by " + connectedPeerID + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
     public void chokeByLog(int connectedPeerID) throws IOException {
-
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " is choked by " + connectedPeerID + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
-    public void receiveHaveLog(int connectedPeerID) throws IOException {
-
+    public void receiveHaveLog(int connectedPeerID, int pieceIndex) throws IOException {
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " received the  ‘have’ message from " + connectedPeerID + " for the piece " + pieceIndex + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
     public void receiveInterestedLog(int connectedPeerID) throws IOException {
-
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " received the ‘interested’ message from " + connectedPeerID + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
     public void receiveNotInterestedLog(int connectedPeerID) throws IOException {
-
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " received the ‘not interested’ message from " + connectedPeerID + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
-    public void downloadPieceFromLog(int connectedPeerID) throws IOException {
-
+    public void downloadPieceFromLog(int connectedPeerID, int pieceIndex, int pieceCount) throws IOException {
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " has downloaded the piece " + pieceIndex + " from " + connectedPeerID +
+                ". Now the number of pieces it has is " + pieceCount + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
     // can call this by running is bit field full when receiving a piece and printing if true
-    public void completeFileDownloadLog(int connectedPeerID) throws IOException {
-
+    public void completeFileDownloadLog() throws IOException {
+        String toWrite  = generateCurrentDateTime() + " : Peer " + this.peerID + " has downloaded the complete file" + ".\n";
+        this.fileWriter.write(toWrite);
     }
 
     // to be called when the Peer thread shuts down
